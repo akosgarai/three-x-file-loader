73e4a9732297eda66ac7a47f8de209ff92bdc9cd
akosgarai <szlem01@gmail.com>
2022-08-16 21:57:47 +0200

    ReadInt stringutil function

--- src/utils/string.js
+++ src/utils/string.js
@@ -4,4 +4,32 @@ module.exports = {
 		const countAsSpace = [' ', '\f', '\n', '\r', '\t', '\v'];
 		return countAsSpace.indexOf(currChar) > -1;
 	},
+	readInteger: function(fullText) {
+		let result = {
+			value: 0,
+			valueLength: 0,
+		};
+		let isNegative = false;
+
+		if (fullText[result.valueLength] == '-') {
+			isNegative = true;
+			++result.valueLength;
+		}
+		if (isNaN(parseInt(fullText[result.valueLength], 10))) {
+			throw 'Number expected.'
+		}
+		while (result.valueLength < fullText.length) {
+			let currentDigit = parseInt(fullText[result.valueLength], 10);
+			if (isNaN(currentDigit)) {
+				break;
+			}
+			result.value = result.value * 10 + currentDigit;
+			++result.valueLength;
+		}
+		if (isNegative) {
+			result.value = -1*result.value;
+		}
+
+		return result;
+	},
 };
--- src/utils/string.spec.js
+++ src/utils/string.spec.js
@@ -15,4 +15,39 @@ describe('String Utils', () => {
 			});
 		});
 	});
+	describe('readInteger', () => {
+		const testDataInt = [
+			{
+				text: '10a',
+				expectedInt: 10,
+				expectedLen: 2,
+			},
+			{
+				text: '0',
+				expectedInt: 0,
+				expectedLen: 1,
+			},
+			{
+				text: '1',
+				expectedInt: 1,
+				expectedLen: 1,
+			},
+			{
+				text: '-1',
+				expectedInt: -1,
+				expectedLen: 2,
+			},
+		];
+		const testDataNotInt = ['wrong', '-wrong', ''];
+		test('General Tests', () => {
+			testDataInt.forEach(item => {
+				const result = StringUtils.readInteger(item.text);
+				expect(result.value).toBe(item.expectedInt);
+				expect(result.valueLength).toBe(item.expectedLen);
+			});
+			testDataNotInt.forEach(item => {
+				expect(() => { StringUtils.readInteger(item); }).toThrow('Number expected.');
+			});
+		});
+	});
 });
