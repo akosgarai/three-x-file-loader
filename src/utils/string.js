5d3c589022bea24dd7b92013d5679d20efc90a0c
akosgarai <szlem01@gmail.com>
2022-08-29 14:29:10 +0200

    readFloat string util function

--- src/utils/string.js
+++ src/utils/string.js
@@ -32,4 +32,47 @@ module.exports = {
 
 		return result;
 	},
+	// read a floating point number. Input is the full text of the rest of the model file.
+	// The output is an object with the value and the length of the text read.
+	// The value is the floating point number.
+	// The length is the number of characters read.
+	readFloat: function(fullText) {
+		let result = {
+			value: 0,
+			valueLength: 0,
+		};
+		let isNegative = false;
+		if (fullText[result.valueLength] == '-') {
+			isNegative = true;
+			++result.valueLength;
+		}
+		if (isNaN(parseInt(fullText[result.valueLength], 10))) {
+			throw 'Number expected.'
+		}
+		while (result.valueLength < fullText.length) {
+			let currentDigit = parseInt(fullText[result.valueLength], 10);
+			if (isNaN(currentDigit)) {
+				break;
+			}
+			result.value = result.value * 10 + currentDigit;
+			++result.valueLength;
+		}
+		if (isNegative) {
+			result.value = -1*result.value;
+		}
+		if (fullText[result.valueLength] == '.') {
+			++result.valueLength;
+			let decimal = 0.1;
+			while (result.valueLength < fullText.length) {
+				let currentDigit = parseInt(fullText[result.valueLength], 10);
+				if (isNaN(currentDigit)) {
+					break;
+				}
+				result.value = result.value + decimal * currentDigit;
+				decimal = decimal * 0.1;
+				++result.valueLength;
+			}
+		}
+		return result;
+	}
 };
--- src/utils/string.spec.js
+++ src/utils/string.spec.js
@@ -50,4 +50,65 @@ describe('String Utils', () => {
 			});
 		});
 	});
+	// Testcase for the readFloat function.
+	describe('readFloat', () => {
+		// test data definition for 8 test cases. array of objects with the following properties:
+		// text: the text to read
+		// expectedValue: the expected value of the read float
+		// expectedLen: the expected length of the read float
+		const testDataFloat = [
+			{
+				text: '10.0',
+				expectedValue: 10.0,
+				expectedLen: 4,
+			},
+			{
+				text: '0.0',
+				expectedValue: 0.0,
+				expectedLen: 3,
+			},
+			{
+				text: '1.0',
+				expectedValue: 1.0,
+				expectedLen: 3,
+			},
+			{
+				text: '-1.0',
+				expectedValue: -1.0,
+				expectedLen: 4,
+			},
+			{
+				text: '1.0001',
+				expectedValue: 1.0001,
+				expectedLen: 6,
+			},
+			{
+				text: '1.0001bob',
+				expectedValue: 1.0001,
+				expectedLen: 6,
+			},
+			{
+				text: '1',
+				expectedValue: 1,
+				expectedLen: 1,
+			},
+			{
+				text: '-1',
+				expectedValue: -1,
+				expectedLen: 2,
+			},
+		];
+		// Testcases for not float values.
+		const testDataNotFloat = ['wrong', '-wrong', ''];
+		test('General Tests', () => {
+			testDataFloat.forEach(item => {
+				const result = StringUtils.readFloat(item.text);
+				expect(result.value).toBe(item.expectedValue);
+				expect(result.valueLength).toBe(item.expectedLen);
+			} );
+			testDataNotFloat.forEach(item => {
+				expect(() => { StringUtils.readFloat(item); }).toThrow('Number expected.');
+			});
+		});
+	});
 });
