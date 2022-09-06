51ce4c601a662d5ae5ab20fd6ad4eaaedf900965
akosgarai <szlem01@gmail.com>
2022-09-06 15:37:53 +0200

    Internal classes to eliminate three dependency from tests

---
+++ src/utils/types.js
@@ -0,0 +1,16 @@
+module.exports = {
+	Vector3: class {
+		constructor(x, y, z) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	},
+	Color: class {
+		constructor(r, g, b) {
+			this.r = r;
+			this.g = g;
+			this.b = b;
+		}
+	},
+};
