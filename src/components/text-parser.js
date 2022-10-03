100f0810676a92193df0a71f81e079e9852c4616
akosgarai <szlem01@gmail.com>
2022-10-03 22:50:04 +0200

    Text parser application

---
+++ src/components/text-parser.js
@@ -0,0 +1,140 @@
+const Parsers = require('../utils/parsers');
+const StringUtils = require('../utils/string');
+const Types = require('../utils/types');
+
+module.exports = class TextParser {
+
+	// It contains the text of the .X file
+	fileContent;
+	// It points to the index of the lastly processed file content.
+	readUntil;
+	// It stores the processed line numbers.
+	lineNumber;
+	// It stores the Scene data.
+	exportScene;
+	// textureBaseDir is the main part of the current url.
+	textureBaseDir;
+	// The texture loader THREE.textureLoader;
+	texLoader;
+
+	constructor(text, textureBaseDir, texLoader) {
+		this.fileContent = text;
+		this.readUntil = 0;
+		this.lineNumber = 0;
+		// the first line is the format definition. Not connected to the model content.
+		this.readUntil += StringUtils.readUntilEndOfLine(this.fileContent.substring(this.readUntil));
+		this.lineNumber++;
+
+		this.exportScene = new Types.Scene();
+		this.textureBaseDir = textureBaseDir;
+		this.texLoader = texLoader;
+	}
+
+	parse() {
+		while (this.readUntil < this.fileContent.length) {
+			const objectName = StringUtils.getNextToken(this.fileContent.substring(this.readUntil));
+			this.readUntil += objectName.valueLength;
+			this.lineNumber += objectName.lines;
+			if (objectName.nodeData == '') {
+				break;
+			}
+			this._parseObjectBasedOnName(objectName);
+			const skipped = StringUtils.readUntilNextNonWhitespace(this.fileContent.substring(this.readUntil));
+			this.readUntil += skipped.valueLength;
+			this.lineNumber += skipped.lines;
+		}
+		return this.exportScene;
+	}
+
+	_parseObjectBasedOnName(objectName) {
+		switch (objectName) {
+			case 'template':
+				this._parseTemplateObject();
+				break;
+			case 'Frame':
+				this._parseFrameObject();
+				break;
+			case 'Mesh':
+				this._parseMeshObject();
+				break;
+			case 'AnimTicksPerSecond':
+				this._parseAnimTicksPerSecondObject();
+				break;
+			case 'AnimationSet':
+				this._parseAnimationSetObject();
+				break;
+			case 'Material':
+				this._parseMaterialObject();
+				break;
+			case '}':
+				break;
+			default:
+				this._parseUnknownObject();
+				break;
+		}
+	}
+
+	_parseTemplateObject() {
+		const template = Parsers.templateNode(this.fileContent.substring(this.readUntil));
+		this.readUntil += template.valueLength;
+		this.lineNumber += template.lines;
+	}
+	_parseFrameObject() {
+		const frame = Parsers.frameNode(this.fileContent.substring(this.readUntil));
+		this.readUntil += template.valueLength;
+		this.lineNumber += template.lines;
+		if (this.exportScene.rootNode == null) {
+			this.exportScene.rootNode = frame.nodeData;
+		} else {
+			// We already have a root node. We need to create a new root node and add the previous root node as a child.
+			// Name the new root node as '$dummy_root', as it is called in the assimp implementation.
+			// If the root node is the dummy root, then we can just add the new frame as a child.
+			if (this.exportScene.rootNode.name == '$dummy_root') {
+			} else {
+				// We need to create a new root node.
+				const newRoot = new Types.Node(null);
+				newRoot.name = '$dummy_root';
+				this.exportScene.rootNode.parent = newRoot;
+				newRoot.childrenNodes.push(this.exportScene.rootNode);
+				this.exportScene.rootNode = newRoot;
+			}
+			frame.nodeData.parentNode = this.exportScene.rootNode;
+			this.exportScene.rootNode.childrenNodes.push(frame.nodeData);
+		}
+	}
+	// parse a mesh object definition based on the assimp xfile parser.
+	// The assimp implementation is located in this link: https://github.com/assimp/assimp/blob/master/code/AssetLib/X/XFileParser.cpp#L389-L445
+	// The mesh has to be base on THREE.Mesh.
+	_parseMeshObject(parentNode) {
+		const mesh = Parsers.meshNode(this.fileContent.substring(this.readUntil));
+		this.readUntil += mesh.valueLength;
+		this.lineNumber += mesh.lines;
+		this.exportScene.meshes.push(mesh.nodeData);
+	}
+	// Parse a material object definition based on the assimp xfile parser logic
+	// The assimp implementation is located in this link: https://github.com/assimp/assimp/blob/master/code/AssetLib/X/XFileParser.cpp#L655-L692
+	// The extracted material is stored in this.exportScene.materials
+	_parseMaterialObject() {
+		const material = Parsers.materialNode(this.fileContent.substring(this.readUntil));
+		this.readUntil += material.valueLength;
+		this.lineNumber += material.lines;
+		this.exportScene.materials.push(material.nodeData);
+	}
+	_parseAnimTicksPerSecondObject() {
+		const animTicksPerSecond = Parsers.animTicksPerSecondNode(this.fileContent.substring(this.readUntil));
+		this.readUntil += animTicksPerSecond.valueLength;
+		this.lineNumber += animTicksPerSecond.lines;
+		this.exportScene.animTicksPerSecond = animTicksPerSecond.nodeData;
+	}
+	_parseAnimationSetObject() {
+		const animationSet = Parsers.animationSetNode(this.fileContent.substring(this.readUntil));
+		this.readUntil += animationSet.valueLength;
+		this.lineNumber += animationSet.lines;
+		this.exportScene.animations.push(animationSet.nodeData);
+	}
+	_parseUnknownObject() {
+		const unknown = Parsers.unknownNode(this.fileContent.substring(this.readUntil));
+		this.readUntil += unknown.valueLength;
+		this.lineNumber += unknown.lines;
+	}
+}
